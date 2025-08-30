51d67346830e9547766312b2c6f1e2307d9bb6f5
yulog <29723448+yulog@users.noreply.github.com>
2025-08-30 16:51:57 +0900

    ci(human): debug linux arm64

--- misskey-reactions/magefiles/magefile.go
+++ misskey-reactions/magefiles/magefile.go
@@ -111,7 +111,7 @@ func Cross(goos, arch string) {
 		// cflags, _ := sh.Output("pkg-config --cflags x11 xcursor xi xinerama xrandr xxf86vm gl alsa")
 		// ldflags, _ := sh.Output("pkg-config --libs x11 xcursor xi xinerama xrandr xxf86vm gl alsa")
 		os.Setenv("CGO_CFLAGS", "-isystem /usr/include")
-		// os.Setenv("CGO_LDFLAGS", ldflags)
+		os.Setenv("CGO_LDFLAGS", "-L/usr/lib/aarch64-linux-gnu")
 		// os.Setenv("EXTRA_LDFLAGS", "-linkmode=external -extldflags=-static")
 	}
 	sh.Run("go", "build", "-v", "-o", BIN, BUILD_TARGET)
